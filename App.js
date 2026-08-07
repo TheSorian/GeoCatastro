a39a54c33a598a343b3b257f0e634910096230da
Gabriel <gsm.sanz@gmail.com>
2026-08-07 22:56:49 +0200

    fix: missing closing View tag in App.js

--- App.js
+++ App.js
@@ -716,6 +716,7 @@ export default function App() {
                     );
                   })}
                 </ScrollView>
+                </View>
               )}
             </>
           ) : (
